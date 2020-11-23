d9c9efac02e07df6caee7bf73155681b1c420103
Landon <34496757+groans@users.noreply.github.com>
2020-11-22 19:56:32 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,53 +1,85 @@
 import requests
-import os
 import string
 import json
-import traceback
-import socket
-import concurrent
+import sys
+from termcolor import colored
+from threading import Thread
 from itertools import cycle
-from multiprocessing import Pool
+from requests.exceptions import ConnectionError
+from requests.packages.urllib3.exceptions import MaxRetryError
+from requests.packages.urllib3.exceptions import ProxyError as urllib3_ProxyError
 
-def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
-def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+# Old JSON support. Moved to plaintext to make more user-friendly.
+#names = json.loads(open('accounts.json').read())
+#proxies = json.loads(open('proxies.json').read())
 
+with open("accounts.txt", "r+") as f:
+    names = [x.strip() for x in f.readlines()]
 
-names = json.loads(open('accounts.json').read())
-proxies = json.loads(open('proxies.json').read())
-url = 'https://namemc.com/profile/'
+with open("proxies.txt", "r+") as f:
+    proxies = [x.strip() for x in f.readlines()]
 
-def checkMigration(name):
-	proxy_pool = cycle(proxies)
-	while True:
-		try:
-			proxy = next(proxy_pool)
-			r = requests.get(url + name, proxies={"http": proxy, "https": proxy})
-			print("Checking " + name + " for unmigrated status.")
-			if "Unmigrated" in r.text:
-				file = open("accounts.txt", "a")
-				file.write(name)
-				file.close()
-				prGreen("ACCOUNT UNMIGRATED :) (accounts.txt))")
-			else:
-				prRed("ACCOUNT MIGRATED :(")
-		except:
-			print("Connection error. Retrying...")
-			proxy = next(proxy_pool)
-			continue
-		break
+uuidURL = "https://api.mojang.com/users/profiles/minecraft/"
+migratedURL = "https://sessionserver.mojang.com/session/minecraft/profile/"
+total = len(names)
+num = 1
+proxy_pool = cycle(proxies)
+
+def checkMigration(name, uuid, proxy):
+	attempt = num + 1
+	percentage = round(attempt / total, 3)
+	percentage = percentage * 100
+	urlResponse = requests.get(migratedURL + uuid, proxies={"http": "http://"+proxy, "https": "https://"+proxy}, timeout=2).json()
+	try:
+		isLegacy = urlResponse["legacy"]
+		file = open("unmigrated.txt", "a")
+		file.write(name+"\n")
+		file.close()
+		print("(" + str(attempt) + "/" + str(total) + "| " + str(percentage) + "%) Checking " + name + " for unmigrated status... [" + colored("UNMIGRATED", "green") + "] Proxy: " + proxy)
+	except (KeyError, IndexError):
+		print("(" + str(attempt) + "/" + str(total) + "| " + str(percentage) + "%) Checking " + name + " for unmigrated status... [" + colored("MIGRATED", "red") + "] Proxy: " + proxy)
+
+def getUUID(name, proxy, i=[0]):
+	attempt = num + 1
+	percentage = round(attempt / total, 3)
+	percentage = percentage * 100
+	try:
+		urlResponse = requests.get(uuidURL + name, proxies={"http": "http://"+proxy, "https": "https://"+proxy}, timeout=10).json()
+		uuid = urlResponse["id"]
+		checkMigration(name, uuid, proxy)
+		i[0] = 0
+	except (ValueError, KeyError, IndexError):
+		i[0] = 0
+		print("(" + str(attempt) + "/" + str(total) + "| " + str(percentage) + "%) Checking " + name + " for unmigrated status... [" + colored("INVALID", "red") + "] Proxy: " + proxy)
+	except (ConnectionError, OSError):
+		i[0]+=1
+		if i[0] == len(proxies):
+			sys.exit("No proxies could establish a connection. Try new proxies and a larger proxylist.")
+		#print("Proxy error (rate limited?). Cycling proxy.")
+		getUUID(name, getWorkingProxy())
+
+def getWorkingProxy():
+  proxy = next(proxy_pool)
+  try:
+    request = requests.get("http://ipinfo.io/json", proxies={"http": "http://"+proxy, "https": "https://"+proxy}, timeout=1).json()
+    return proxy
+  except Exception as e:
+    return getWorkingProxy()
 
 if __name__ == '__main__':
+	thread_list = []
+	count = 0
 	print("")
 	print("Checking all cape accounts for 'unmigrated' status...")
-	print("Made by Landon. Landon#1000, @Religion on MC-Market.")
+	print("Made by Landon. Landon#1718, @Religion on MC-Market.")
 	print("")
-	prGreen("All usernames loaded! (accounts.json)")
-	prGreen("All proxies loaded! (proxies.json)")
-	print("Proxies are being cycled...")
-	pool = Pool()
-	pool.map(checkMigration, names)
-
-#if __name__ == '__main__':
-	#executor = concurrent.futures.ProcessPoolExecutor(10)
-	#futures = [executor.submit(checkMigration, name) for name in names]
-	#concurrent.futures.wait(futures)
+	print(colored("All usernames loaded! (accounts.txt)", "green"))
+	print(colored("All proxies loaded! (proxies.txt)", "green"))
+	for name in names:
+	  thread_list.append(Thread(target=getUUID, args=(name, getWorkingProxy())))
+	  thread_list[len(thread_list)-1].start()
+	  count += 1
+	  num += 1
+	for x in thread_list:
+	  x.join()
+	print("Completed.")
